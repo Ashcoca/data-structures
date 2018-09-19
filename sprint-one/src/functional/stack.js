3140edf677ea9b0187d04439e1a14fc86dfeb587
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-19 11:17:51 -0700

    nit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //added count variable to see how many times functions are run
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count ++;
   };
 
   someInstance.pop = function() {
+    count --;
   };
 
   someInstance.size = function() {
+    //get keys from Object, check length -- solves report size 0 for new stack
+    // return Object.keys(storage).length;
+
+
+    return count;
   };
 
   return someInstance;
