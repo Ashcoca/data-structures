75ffa9ba912cfd65abbb743a7a7c63730c48a58c
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-19 12:15:49 -0700

    Finished stack! 1 down, like 30 to go!

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,24 +2,34 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-  //added count variable to see how many times functions are run
+  storage = {};
+
+  //assign count to keep track of items being added/removed
   var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //***use storage[count] to be the key for the value!
+    storage[count] = value;
+    //count needs to be below?
     count ++;
+
   };
 
   someInstance.pop = function() {
+    //need to remove topmost element of stack
+    if (count > 0) {
     count --;
+    //assign new var to storage[last item], so we can return cause pop does that
+    var popped = storage[count]
+    //now remove that last item from the storage
+    delete storage[count];
+    return popped
+    // return storage;
+    }
   };
 
   someInstance.size = function() {
-    //get keys from Object, check length -- solves report size 0 for new stack
-    // return Object.keys(storage).length;
-
-
     return count;
   };
 
