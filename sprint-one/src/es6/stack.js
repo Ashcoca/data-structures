8e639b30e4d4899b7cbf2a29ec51f1f0ca23bc6a
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-19 21:51:47 -0700

    Finished through es6 stack! Only one more to go!

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,29 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  constructor() {
+
+  constructor(storage, count) {
+
+    this.storage = {};
+    this.count = 0;
+
+  }
+
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if (this.size() > 0) {
+      var data = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count--;
+      return data;
+
+    }
+  }
+
+  size() {
+    return this.count;
   }
 
 }
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.last = 0;
+  this.newest = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.last] = value;
+  this.last++;
+},
 
+Queue.prototype.dequeue = function() {
+  if (this.size() > 0) {
+    var data = this.storage[this.newest];
+    delete this.storage[this.newest];
+    this.newest++;
+    return data;
+  }
+},
+
+Queue.prototype.size = function() {
+  return this.last - this.newest;
+}
+
+// var someInstance = new Queue();
