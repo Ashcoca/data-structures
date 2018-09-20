f9c25cfd871de48564b523ecd54cb98bb4458903
Ashcoca <ashvincoca@gmail.com>
2018-09-20 11:26:05 -0700

    Added notes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,27 +1,54 @@
 var LinkedList = function() {
   var list = {};
+//[6] -> [3] -> [4] -> [2] -> [1]
+//head refers to first box [6]
+//head.next refers to second item - []
+//head.next.data refers to data of next entry [3]
+//head.next.next.data refers to [4]
+//**can only go left to right
   list.head = null;
   list.tail = null;
 
+_.extend(list, Node);
+
   list.addToTail = function(value) {
+    list.Node.node.value = value;
+    console.log(list.Node.node);
+    //conditional see if list.tail is null
+      //add current node.value to var
+      //add value to node
+      //change node.next to previous node.value
+    //conditional see if list.head is null
+      //add value to node
+      //
   };
 
   list.removeHead = function() {
   };
 
   list.contains = function(target) {
+  //maybe loop through list to see if target is in there?
+  //return true if it is?
+  //figure way to step through 1 by 1 otherwise?
   };
 
-  return list;
-};
 
+  return list;
+};                                           
+
+
+//we can assign head and tail to different nodes
+//each node has two properties: value, next
+//each time we add something we change the value of next to the
+//previous node
 var Node = function(value) {
   var node = {};
 
   node.value = value;
-  node.next = null;
+  node.next =  null;
 
   return node;
+
 };
 
 /*
