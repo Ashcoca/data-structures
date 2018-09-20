1ec6d28b1912899cffbb0905d47ab81966b1c27a
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-19 18:34:51 -0700

    Working on shareed

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,4 +1,5 @@
 var Stack = function(storage, count) {
+  //create an object with all our editeable values
   var someInstance = {
     storage: {},
     count: 0
@@ -8,23 +9,30 @@ var Stack = function(storage, count) {
   return someInstance;
 };
 
-//extend methods to our stack
+//extend methods to our Stack
 var extend = function(obj, methods) {
   for (var key in methods) {
     obj[key] = methods[key];
   }
 };
 
+//here's where we'll store all the functions that we'll copy over to Stack
 var stackMethods = {
 
+  //all the 'this' refer to the object (someInstance)
+
   //key value pair because this is an OBJECT,,,
-  push: function(value){
+  push: function(value) {
+    //object.storage[current count]
     this.storage[this.count] = value;
+    //increment count so the next value added will be at the next key
     this.count ++;
   },
 
-  pop: function(){
+  pop: function() {
     if (this.size() > 0) {
+      //needs to be this.count-1 to remove last key/value in storage
+      //this is because count was incremented the last time something was added
       var result = this.storage[this.count - 1];
       delete this.storage[this.count - 1];
       this.count --;
@@ -32,7 +40,7 @@ var stackMethods = {
     }
   },
 
-  size:  function(){
+  size: function() {
     return this.count;
   }
 
