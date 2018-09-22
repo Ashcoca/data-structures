6b0039bce21acf20e61954581f8d0fbad6dd5db9
Ashcoca <ashvincoca@gmail.com>
2018-09-21 19:51:57 -0700

    Pushing through two tests on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,40 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
-};
+
+}; 
+
+
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = {};
+  child.value = value;
+
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+
 };
 
+//start with root
+//determine if node needs to go in left or right branch
+//each node needs to point at the next child
+//how do we figure when to make a child vs a sibling?
+
+//every node has a left node and a right node
+//some of those children nodes will be null
+
+//this note below refers to binary search trees
+//left node is less than the root node *and* less than all of the right nodes
+//
 
 
 /*
