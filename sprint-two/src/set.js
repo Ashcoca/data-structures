1410ad5a6a79e0e19994388bc2d4bdca1d6bd422
Ashcoca <ashvincoca@gmail.com>
2018-09-22 13:56:56 -0700

    Finished sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,40 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {}; // fix me
+  //this set is a placeholder for what you'll call when the new Set is instantiated
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-
-
+//are we basically just pushing the item to the storage array?
+//we NEED to do a check to make sure the item doesn't already exist
+//maybe use set.includes(item)?
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+//just iterate over storage array to find the item?
+//want to try using a for of loop to experiment?
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
+
 };
 
 setPrototype.remove = function(item) {
+//iterate and delete if the index matches the item?
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      delete this._storage[i];
+    }
+  }
 };
 
 /*
